974adce0414671db60e264e96fc228e55588946f
Fahrurrazi <fahrurrazi@hreasilygroup.com>
2026-09-02 05:27:15 +0700

    Application update status

--- sewa-backend/sewa/views/application_view.py
+++ sewa-backend/sewa/views/application_view.py
@@ -1,8 +1,13 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
 
-from sewa.models import Application
+
+from sewa.models import Application, ApplicationStatus
 from sewa.models.user import UserRole
 from sewa.serializers import ApplicationSerializer
 
@@ -28,3 +33,38 @@ class ApplicationView(viewsets.ModelViewSet[Application]):
 
         q = q.order_by('-created_at')
         return q
+
+    @action(detail=True, methods=['post'])
+    def update_status(self, request, pk=None):
+        allowed_status = [ApplicationStatus.ACCEPTED, ApplicationStatus.REJECTED]
+        status_val = request.POST.get("status")
+        reject_reason = request.POST.get("reject_reason")
+
+        application = get_object_or_404(Application, id=pk)
+
+        if application.property.owner != request.user:
+            return Response(
+                {"error": "Entity Not found"},
+                status=status.HTTP_404,
+            )
+
+        if status_val is None and status_val not in allowed_status:
+            return Response(
+                {"error": "Invalid status"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if application.status != ApplicationStatus.REQUESTED:
+            return Response(
+                {"error": "Cannot approve or reject again"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        application.status = status_val
+
+        if status_val == ApplicationStatus.REJECTED:
+            application.reject_reason = reject_reason
+
+        application.save()
+
+        return Response({"status": "success"})
